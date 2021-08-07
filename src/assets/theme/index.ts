5bcfc1a91b84eaa5e550468fc7730ddcc2c7b40a
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-08-08 00:29:35 +0700

    feat: change theme based on design

---
+++ src/assets/theme/index.ts
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+
+export const COLORS = {
+  primary: '#0F3E51',
+  secondary: '#245F71',
+  darkblue: '#5BA3B3',
+  blue: '#A2D8DE',
+  lightblue: '#E1F5F6',
+  lightgray: '#FAFAFA',
+  white: '#FFFFFF',
+
+  placeholder: '#5BA3B3'
+};
+
+export const FONTS = {
+  title: css`
+    font-size: 2rem;
+    font-weight: 700;
+    font-family: 'Noto Sans', sans-serif;
+  `,
+  h1: css`
+    font-size: 1.5rem;
+    font-weight: 700;
+    font-family: 'Noto Sans', sans-serif;
+  `,
+  h2: css`
+    font-size: 1rem;
+    font-family: 'Noto Sans', sans-serif;
+  `,
+  p: css`
+    font-size: 0.6875rem;
+    font-family: 'Noto Sans', sans-serif;
+  `
+};
